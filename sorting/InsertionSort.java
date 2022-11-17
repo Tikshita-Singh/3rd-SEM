71edd943089743a8371831a05d631c0bf1288009
Tikshita Singh <96276437+Tikshita-Singh@users.noreply.github.com>
2022-11-17 12:04:53 +0530

    Update InsertionSort.java

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -21,17 +21,15 @@ public class InsertionSort {
 	}
 	public static void insertion_sort(int arr[])
 	{
-		for(int i = 0;i < arr.length-1;i++)
+		for(int i = 1;i < arr.length;i++)
 		{
-			for(int j = i+1;j > 0;j--)
-			{
-				if(arr[j] < arr[j-1])
-				{
-					int temp = arr[j];
-					arr[j] = arr[j-1];
-					arr[j-1] = temp;
-				}
+			int current = arr[i];
+			int j = i - 1;
+			while(j >= 0 && current < arr[j]) {
+				arr[j+1] = arr[j];
+				j--;
 			}
+			arr[j+1] = current;
 		}
 	}
 }
